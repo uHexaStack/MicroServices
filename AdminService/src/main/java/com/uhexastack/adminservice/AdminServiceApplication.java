00ec606df393f8ba5926224830e97f217ca2badf
Daniel Del Castillo <teocchiii@gmail.com>
2025-06-07 17:27:17 -0500

    feat(admin): changed the dependencies to use eureka server

--- AdminService/src/main/java/com/uhexastack/adminservice/AdminServiceApplication.java
+++ AdminService/src/main/java/com/uhexastack/adminservice/AdminServiceApplication.java
@@ -3,11 +3,12 @@ package com.uhexastack.adminservice;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 
 @EnableAdminServer
 @SpringBootApplication
+@EnableEurekaServer
 public class AdminServiceApplication {
 
     public static void main(String[] args) {
