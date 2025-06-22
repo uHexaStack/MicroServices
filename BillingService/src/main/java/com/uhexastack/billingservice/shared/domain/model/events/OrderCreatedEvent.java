048e5a92d619996bd0c0581888b20a8f4457bac2
Daniel Del Castillo <teocchiii@gmail.com>
2025-06-21 21:45:01 -0500

    chore: crrated billing service initial commit

---
+++ BillingService/src/main/java/com/uhexastack/billingservice/shared/domain/model/events/OrderCreatedEvent.java
@@ -0,0 +1,8 @@
+package com.uhexastack.billingservice.shared.domain.model.events;
+
+import java.util.Map;
+
+public record OrderCreatedEvent(
+    Long orderId,
+    Map<Long, Integer> productQuantities // inventoryItemId -> quantity
+) {}
