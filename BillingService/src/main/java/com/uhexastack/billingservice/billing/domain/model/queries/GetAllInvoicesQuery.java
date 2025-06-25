777c41026e546653186e7b67fcdb95dfb16d94ae
Daniel Del Castillo <teocchiii@gmail.com>
2025-06-25 00:48:27 -0500

    feat(billing): created the billing initial commit

---
+++ BillingService/src/main/java/com/uhexastack/billingservice/billing/domain/model/queries/GetAllInvoicesQuery.java
@@ -0,0 +1,10 @@
+package com.uhexastack.billingservice.billing.domain.model.queries;
+
+/**
+ * Query for retrieving all invoices.
+ * <p>
+ * This query is used to fetch a collection of all invoices in the system.
+ * </p>
+ */
+public record GetAllInvoicesQuery() {
+}
